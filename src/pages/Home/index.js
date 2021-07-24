2b97b858495325af165180b0c81ff2e5bce36651
MRSS <mateusyasd@hotmail.com>
2021-07-24 00:21:44 -0300

    fixed the tag that was wrong

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,8 +7,8 @@ export default function Home() {
      <p className="helloWorld">Hi, this is my website.</p>
      <div className="myWork">
        <div className="desc">Check out my work:</div>
-       <Link to="/webdev">Web developmment</a>
-       <Link to="/games">Games</a>
+       <Link to="/webdev">Web developmment</Link>
+       <Link to="/games">Games</Link>
      </div>
 
     </div>
