9c274769b3ff2e09d61c608e4a5a38c96f01b504
MRSS <mateusyasd@hotmail.com>
2021-07-24 01:33:59 -0300

    corrected divs

--- src/pages/WebDev/index.js
+++ src/pages/WebDev/index.js
@@ -5,15 +5,21 @@ export default function WebDev() {
     <div className="WebDev">
       WebDev
       My websites:
+      <div>
       <a href="https://linkshortener.sites.starmain.dev">
       linkshortener.sites.starmain.dev</a>
       <br />(based on
         <a href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G">
       this challenge</a> from frontendmentor)
+      </div>
+      <div>
       <a href="https://listadecompras.sites.starmain.dev">
       listadecompras.sites.starmain.dev</a>
+      </div>
+      <div>
       <a href="https://loremipsum.sites.starmain.dev">
       loremipsum.sites.starmain.dev</a>
+      </div>
     </div>
   )
 }
