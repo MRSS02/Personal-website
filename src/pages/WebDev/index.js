06dfa6310c2222677ad621463bc8399606ed1049
MRSS <mateusyasd@hotmail.com>
2021-07-23 21:40:41 -0300

    updated links

--- src/pages/WebDev/index.js
+++ src/pages/WebDev/index.js
@@ -4,6 +4,16 @@ export default function WebDev() {
   return (
     <div className="Games">
       WebDev
+      My websites:
+      <a href="https://linkshortener.sites.starmain.dev">
+      linkshortener.sites.starmain.dev</a>
+      <br />(based on
+        <a href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G">
+      this challenge</a> from frontendmentor)
+      <a href="https://listadecompras.sites.starmain.dev">
+      listadecompras.sites.starmain.dev</a>
+      <a href="https://loremipsum.sites.starmain.dev">
+      loremipsum.sites.starmain.dev</a>
     </div>
   )
 }
