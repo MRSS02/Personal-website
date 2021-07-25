4687121d9ed71be5609c5cf63bf2b064254e10f1
MRSS <mateusyasd@hotmail.com>
2021-07-25 01:51:46 -0300

    changed a few padding stuff

--- src/pages/Sites/index.js
+++ src/pages/Sites/index.js
@@ -5,19 +5,22 @@ export default function Sites() {
     <div className="Sites">
       <h1>My websites: </h1>
       <div className="instance">
-        <a href="https://linkshortener.sites.starmain.dev">
-        linkshortener.sites.starmain.dev</a>
+        <h2>Link Shortener</h2>
+        <a className="projectLink" href="https://linkshortener.sites.starmain.dev">
+        Check it out!</a>
         <br />(based on
           <a href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G"> this
           challenge</a> from frontendmentor)
       </div>
       <div className="instance">
-        <a href="https://listadecompras.sites.starmain.dev">
-        listadecompras.sites.starmain.dev</a>
+        <h2>Lista de Compras (Shop List)</h2>
+        <a className="projectLink" href="https://listadecompras.sites.starmain.dev">
+        Check it out!</a>
       </div>
       <div className="instance">
-      <a href="https://loremipsum.sites.starmain.dev">
-      loremipsum.sites.starmain.dev</a>
+         <h2>Gerador de Lorem Ipsum (Lorem Ipsum Generator)</h2>
+      <a className="projectLink" href="https://loremipsum.sites.starmain.dev">
+      Check it out!</a>
       </div>
       <div className="instance">
        See more on <a href="https://github.com/MRSS02">my Github page!</a>
