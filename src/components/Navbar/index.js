76c0c8863426e9be6d2a6fe14a23600040314982
MRSS <mateusyasd@hotmail.com>
2021-07-31 20:36:31 -0300

    add highlight to routes

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,12 +1,24 @@
 import "./styles.css"
-import { Link } from "react-router-dom"
+import { Link, useLocation } from "react-router-dom"
+
 
 export default function Navbar() {
+  let location = useLocation();
+
   return (
     <div className="Navbar">
-      <Link className="linkInstance" to="/">Home</Link>
-      <Link className="linkInstance" to="/sites">Sites</Link>
-      <Link className="linkInstance" to="/games">Games</Link>
+      <Link className={
+        location.pathname === "/" ? "active linkInstance"
+        : "linkInstance"}
+      to="/">Home</Link>
+      <Link className={
+        location.pathname === "/sites" ? "active linkInstance"
+        : "linkInstance"}
+        to="/sites">Sites</Link>
+      <Link className={
+        location.pathname === "/games" ? "active linkInstance"
+        : "linkInstance"}
+      to="/games">Games</Link>
     </div>
   )
 }
