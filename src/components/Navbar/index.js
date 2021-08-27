388ff53cd888dda6025471d26f1fd192387a219f
MRSS <mateusyasd@hotmail.com>
2021-08-26 22:57:30 -0300

    i was stupid and didn't save the files

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,31 +1,24 @@
 import "./styles.css"
+import { useEffect } from "React"
 import { Link, useLocation } from "react-router-dom"
 
 export default function Navbar() {
   let location = useLocation();
-
-  function fadeInAnimation(event) {
-    console.log(event.target.className)
-    event.target.className += " transition"
-    setTimeout(() => {
-      event.target.className.replace("transition", "")
-    }, 4000);
-  }
 
   return (
     <div className="Navbar">
       <Link className={
         location.pathname === "/" ? "active linkInstance"
         : "linkInstance"}
-      to="/" onClick={(e) => fadeInAnimation(e)}>Home</Link>
+      to="/">Home</Link>
       <Link className={
         location.pathname === "/sites" ? "active linkInstance"
         : "linkInstance"}
-        to="/sites" onClick={(e) => fadeInAnimation(e)}>Sites</Link>
+        to="/sites">Sites</Link>
       <Link className={
         location.pathname === "/games" ? "active linkInstance"
         : "linkInstance"}
-      to="/games" onClick={(e) => fadeInAnimation(e)}>Games</Link>
+      to="/games">Games</Link>
     </div>
   )
 }
