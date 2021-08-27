15fe9a34c9777d5d0394cf7d11207db4887fcc00
MRSS <mateusyasd@hotmail.com>
2021-08-26 21:03:51 -0300

    added soe transitions to buttons

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,24 +1,31 @@
 import "./styles.css"
 import { Link, useLocation } from "react-router-dom"
 
-
 export default function Navbar() {
   let location = useLocation();
+
+  function fadeInAnimation(event) {
+    console.log(event.target.className)
+    event.target.className += " transition"
+    setTimeout(() => {
+      event.target.className.replace("transition", "")
+    }, 4000);
+  }
 
   return (
     <div className="Navbar">
       <Link className={
         location.pathname === "/" ? "active linkInstance"
         : "linkInstance"}
-      to="/">Home</Link>
+      to="/" onClick={(e) => fadeInAnimation(e)}>Home</Link>
       <Link className={
         location.pathname === "/sites" ? "active linkInstance"
         : "linkInstance"}
-        to="/sites">Sites</Link>
+        to="/sites" onClick={(e) => fadeInAnimation(e)}>Sites</Link>
       <Link className={
         location.pathname === "/games" ? "active linkInstance"
         : "linkInstance"}
-      to="/games">Games</Link>
+      to="/games" onClick={(e) => fadeInAnimation(e)}>Games</Link>
     </div>
   )
 }
