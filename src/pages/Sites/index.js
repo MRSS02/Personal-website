48dc59895a5f73ae4f217dccff7f725735808d86
MRSS <mateusyasd@hotmail.com>
2023-12-28 15:52:53 -0300

    updates to styling

--- src/pages/Sites/index.js
+++ src/pages/Sites/index.js
@@ -15,36 +15,36 @@ export default function Sites() {
             <a href="https://www.frontendmentor.io/challenges/url-shortening-api-landing-page-2ce3ob-G"> 
               this challenge
             </a> 
-            <span> </ span>from frontendmentor)
+            <span> </span>from frontendmentor)
           </div>
         </div>
         <div className="instance">
-          <h2>Balls (joke website for learning purposes, displays an increasing quantity of sports balls)</h2>
+          <h2 className="subtitle">Balls</h2>
+          <p className="description">(joke website for learning purposes, displays an increasing quantity of sports balls)</p>
           <a className="projectLink" href="https://balls.balls.starmain.dev">
             Check it out!
           </a>
         </div>
         <div className="instance">
-          <h2>Lista de Compras (Shop List)</h2>
+          <h2 className="subtitle">Lista de Compras (Shop List)</h2>
           <a className="projectLink" href="https://listadecompras.starmain.dev">
             Check it out!
           </a>
         </div>
         <div className="instance">
-          <h2>Gerador de Lorem Ipsum (Lorem Ipsum Generator)</h2>
+          <h2 className="subtitle">Gerador de Lorem Ipsum (Lorem Ipsum Generator)</h2>
           <a className="projectLink" href="https://loremipsum.starmain.dev">
             Check it out!
           </a>
         </div>
         <div className="instance">
-          <h2>Hublive (college project)</h2>
+          <h2 className="subtitle">Hublive (college project)</h2>
+          <p className="description">
+            Created with <a href="https://github.com/Edson-G">Edson G.</a> and <a href="">João Victor</a>
+          </p>
           <a className="projectLink" href="https://hublive.vercel.app/">
             Check it out!
-          </a>
-          <div className="description">
-            Created with <a href="https://github.com/Edson-G">Edson G.</a> and <a href="">João Victor</a>
-          </div>
-        </div>
+          </a>        </div>
         <div className="instance">
         <h2>Job-challenge (landing page made for a job application)</h2>
         <a className="projectLink" href="https://job-challenge.starmain.dev/">
