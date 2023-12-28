3cda5473333fc8d5b4b4254d7827f2d074448a06
MRSS <mateusyasd@hotmail.com>
2023-12-28 15:36:46 -0300

    update with newer projects

--- src/pages/Sites/index.js
+++ src/pages/Sites/index.js
@@ -19,6 +19,12 @@ export default function Sites() {
           </div>
         </div>
         <div className="instance">
+          <h2>Balls (joke website for learning purposes, displays an increasing quantity of sports balls)</h2>
+          <a className="projectLink" href="https://balls.balls.starmain.dev">
+            Check it out!
+          </a>
+        </div>
+        <div className="instance">
           <h2>Lista de Compras (Shop List)</h2>
           <a className="projectLink" href="https://listadecompras.starmain.dev">
             Check it out!
@@ -51,6 +57,12 @@ export default function Sites() {
           Check it out!
         </a>
         </div>
+        </div>
+        <div className="instance">
+        <h2>Discord Video Embedder</h2>
+        <a className="projectLink" href="https://video.starmain.dev/">
+          Check it out!
+        </a>
         </div>
         <div className="outsideLink">
         See more details on <a href="https://github.com/MRSS02">my Github page!</a>
